81024dc7116ace6d19e0085085acd02d39a52336
Reece Boyd <rolltidereece@gmail.com>
2016-03-19 16:34:41 -0400

    Added complete Lesson_03

---
+++ Lesson_03/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // random number between 0 and 49
+        int randomNumber = (int) (Math.random() * 50);
+
+        // ~ == bitwise compliement
+        System.out.println("The Random number: " + randomNumber);
+        System.out.println("Its compliment: " + ~randomNumber);
+        // & == bitwise AND
+        System.out.println("ANDing it with 1: " + (randomNumber & 1));
+        // | == bitwise OR
+        System.out.println("ORing it with 1: " + (randomNumber | 1));
+        // ^ == XOR.
+        System.out.println("XORing it with 1: " + (randomNumber ^ 1));
+        // << == left shift
+        System.out.println("Left shifting it once: " + (randomNumber << 1));
+        // >> == right shift
+        System.out.println("Right shifting it once: " + (randomNumber >> 1));
+        // >>> == zero fill shift right
+        // shifted values filled with 0's
+        // Note: there is no zero shifting left.
+        System.out.println("Zero shifting it right once: " + (randomNumber >>> 1));
+
+        int value1 = 5;
+        int value2 = 7;
+
+        // Assigning variable from conditional.
+        int biggestValue = (value1 > value2) ? value1 : value2;
+        System.out.println(biggestValue);
+
+        // How to use switch statements in Java.
+        char theGrade = 'D';
+        switch (theGrade) {
+            case 'a':
+            case 'A':
+                System.out.println("Great job!");
+                break;
+            case 'b':
+            case 'B':
+                System.out.println("Good job!");
+                break;
+            case 'c':
+            case 'C':
+                System.out.println("You passed I guess.");
+                break;
+            case 'd':
+            case 'D':
+                System.out.println("D's get degrees.");
+                break;
+            default:
+                System.out.println("You failed.");
+                break;
+        }
+    }
+}
---
+++ Lesson_03/README.md
@@ -0,0 +1,6 @@
+# Lesson 3
+I learned :
+- Bitwise operators
+- Logical operators
+- Assigning variables from conditionals
+- Switch statements
