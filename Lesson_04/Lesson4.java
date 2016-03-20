d6088623b18bf990ce3b2c809efed646f6b1e0f5
Reece Boyd <rolltidereece@gmail.com>
2016-03-20 14:39:30 -0500

    Added skipped Lesson 4

---
+++ Lesson_04/Lesson4.java
@@ -0,0 +1,9 @@
+public class Lesson4 {
+
+  public static void Main(String[] args)
+  {
+    // for loops
+    // while loops
+    // do while loops
+  }
+}
---
+++ Lesson_04/README.md
@@ -0,0 +1,2 @@
+# Lesson 4
+This one is basically a skipped lesson because it I did not learn any new concepts in this lesson.
